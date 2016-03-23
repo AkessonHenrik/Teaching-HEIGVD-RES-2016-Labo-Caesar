20c358c69f4168b0de5e764d4d7f89eb0b8312b2
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-03-23 12:13:41 +0100

    First commit, includes protocol

--- src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/protocol/Protocol.java
+++ src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/protocol/Protocol.java
@@ -1,11 +1,30 @@
 package ch.heigvd.res.caesar.protocol;
 
+import java.util.Random;
+
 /**
- *
  * @author Olivier Liechti
  */
 public class Protocol {
-  
-  public static final int A_CONSTANT_SHARED_BY_CLIENT_AND_SERVER = 42;
+
+    public static final int PORT = 2205;
+    String message;
+    byte offset;
+
+    public Protocol(String message) {
+        offset = (byte) new Random().nextInt();
+        this.message = message;
+    }
+
+    public byte[] getEncodedMessage() {
+        byte[] messageBytes = message.getBytes();
+        int encodedMessageLength = message.getBytes().length;
+        byte[] encodedMessage = new byte[encodedMessageLength+1];
+        encodedMessage[0] = offset;
+        for(int i = 1; i < encodedMessage.length; i++) {
+            encodedMessage[i] = messageBytes[i-1];
+        }
+        return encodedMessage;
+    }
 
 }
